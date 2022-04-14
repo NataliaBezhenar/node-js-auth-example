d28fb8dcd7ff5dc451f45afcdd987704aad6c0e0
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-04-14 16:46:40 +0200

    add password hashing

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const { Conflict } = require("http-errors");
+const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
 
 const register = async (req, res) => {
@@ -7,8 +8,8 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`User with ${email} already exists`);
   }
-
-  const result = await User.create({ name, email, password });
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const result = await User.create({ name, email, password: hashPassword });
   res.status(201).json({
     status: "success",
     code: 201,
